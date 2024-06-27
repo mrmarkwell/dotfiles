150a1bc78987066ea18159bdc7675317431b2046
Matthew Markwell <markwell@google.com>
2024-06-27 14:31:45 -0700

    Added Trouble, and some other minor changes.

--- nixpkgs/home-manager/modules/nvim-kickstart/init.lua
+++ nixpkgs/home-manager/modules/nvim-kickstart/init.lua
@@ -23,6 +23,8 @@ ln -s ~/path/to/some/google-specific/repo/google.lua ./lua/google.lua
 My own todo list for this config.
 TODO:
 - Investigate snippets.
+- Get "Tab" to select the first suggestion in the command completion list (e.g. :Tele<Tab> should select the first item in the suggestion list).
+- Java tabs are wrong.
 
 NOTE: Reminders of cool functionality.
 - Highlight a region and type 'gc' to toggle converting it into a comment.
@@ -41,6 +43,8 @@ NOTE: Reminders of cool functionality.
   - <leader>e to show the full error/diagnosic message on the current line.
   - <leader>l to open the quickfix list.
 - Use <ctrl>h, j, k, or l to switch between windows.
+- <leader>config to open nvim config in a new buffer.
+- <leader>gc to open nvim google config in a new buffer.
 
 The original kickstart comment header starts here.
 
@@ -296,9 +300,12 @@ vim.opt.hlsearch = true
 vim.keymap.set(
   'n',
   '<esc>',
-  "<esc>:noh<CR>:lua require('notify').dismiss()<CR>",
+  '<esc>:noh<CR>:lua require("notify").dismiss()<CR>',
   { desc = 'Dismiss highlights and notify messages silently when hitting <esc> in normal mode.', silent = true }
 )
+
+vim.keymap.set('n', '<leader>config', '<cmd>e ~/.config/nvim/init.lua<CR>', { desc = 'Open Nvim [Config]' })
+vim.keymap.set('n', '<leader>gc', '<cmd>e ~/.config/nvim/lua/google.lua<CR>', { desc = 'Open Nvim [G]oogle [C]onfig' })
 
 -- NOTE: markwell's keymaps end (kickstart keymaps below).
 
@@ -988,7 +995,8 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {
-          autostart = true,
+          -- We need this to start only when certain conditions are met.
+          autostart = false,
           cmd = {
             'clangd',
             -- Do everything in-memory for performance.
@@ -1164,7 +1172,7 @@ require('lazy').setup({
             luasnip.lsp_expand(args.body)
           end,
         },
-        completion = { completeopt = 'menu,menuone,noinsert' },
+        completion = { completeopt = 'menu,menuone,noselect' },
 
         -- For an understanding of why these mappings were
         -- chosen, you will need to read `:help ins-completion`
@@ -1348,6 +1356,10 @@ require('lazy').setup({
   },
   {
     'rcarriga/nvim-notify',
+    config = function()
+      -- Use notify as the default 'notify' function.
+      vim.notify = require 'notify'
+    end,
   },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
@@ -1419,6 +1431,70 @@ require('lazy').setup({
       --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
+  },
+  {
+    'folke/trouble.nvim',
+    dependencies = 'kyazdani42/nvim-web-devicons',
+    event = 'VeryLazy',
+    config = function()
+      require('trouble').setup {
+        position = 'bottom', -- position of the list can be: bottom, top, left, right
+        height = 10, -- height of the trouble list when position is top or bottom
+        width = 50, -- width of the list when position is left or right
+        icons = true, -- use devicons for filenames
+        mode = 'workspace_diagnostics', -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
+        fold_open = '', -- icon used for open folds
+        fold_closed = '', -- icon used for closed folds
+        group = true, -- group results by file
+        padding = true, -- add an extra new line on top of the list
+        action_keys = {
+          -- key mappings for actions in the trouble list
+          -- map to {} to remove a mapping, for example:
+          -- close = {},
+          close = 'q', -- close the list
+          cancel = '<esc>', -- cancel the preview and get back to your last window / buffer / cursor
+          refresh = 'r', -- manually refresh
+          jump = { '<cr>', '<tab>' }, -- jump to the diagnostic or open / close folds
+          open_split = { '<c-x>' }, -- open buffer in new split
+          open_vsplit = { '<c-v>' }, -- open buffer in new vsplit
+          open_tab = { '<c-t>' }, -- open buffer in new tab
+          jump_close = { 'o' }, -- jump to the diagnostic and close the list
+          toggle_mode = 'm', -- toggle between "workspace" and "document" diagnostics mode
+          toggle_preview = 'P', -- toggle auto_preview
+          hover = 'K', -- opens a small popup with the full multiline message
+          preview = 'p', -- preview the diagnostic location
+          close_folds = { 'zM', 'zm' }, -- close all folds
+          open_folds = { 'zR', 'zr' }, -- open all folds
+          toggle_fold = { 'zA', 'za' }, -- toggle fold of current file
+          previous = 'k', -- preview item
+          next = 'j', -- next item
+        },
+        indent_lines = true, -- add an indent guide below the fold icons
+        auto_open = false, -- automatically open the list when you have diagnostics
+        auto_close = false, -- automatically close the list when you have no diagnostics
+        auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
+        auto_fold = false, -- automatically fold a file trouble list at creation
+        auto_jump = { 'lsp_definitions' }, -- for the given modes, automatically jump if there is only a single result
+        signs = {
+          -- icons / text used for a diagnostic
+          error = '',
+          warning = '',
+          hint = '',
+          information = '',
+          other = '',
+        },
+        use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
+      }
+    end,
+    keys = {
+      { '<leader>d', desc = 'Diagnostics (Trouble)' },
+      { '<leader>dt', '<cmd>TroubleToggle<CR>', desc = 'Trouble Toggle' },
+      { '<leader>dw', '<cmd>Trouble workspace_diagnostics<CR>', desc = 'Trouble Workspace Diagnostics' },
+      { '<leader>dd', '<cmd>Trouble document_diagnostics<CR>', desc = 'Trouble Doc Diagnostics' },
+      { '<leader>dl', '<cmd>Trouble loclist<CR>', desc = 'Trouble Loclist' },
+      { '<leader>dq', '<cmd>Trouble quickfix<CR>', desc = 'Trouble Quickfix' },
+      { '<leader>dk', vim.diagnostic.open_float, desc = 'Open Diagnostic Float' },
+    },
   },
   {
     -- debug.lua
@@ -1523,11 +1599,11 @@ require('lazy').setup({
     'mfussenegger/nvim-lint',
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
-      local lint = require 'lint'
-      lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
-      }
-
+      -- local lint = require 'lint'
+      -- lint.linters_by_ft = {
+      --   markdown = { 'markdownlint' },
+      -- }
+      --
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
       -- instead set linters_by_ft like this:
       -- lint.linters_by_ft = lint.linters_by_ft or {}
