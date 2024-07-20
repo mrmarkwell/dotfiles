f8705a32ed799ca3b42416da717e869918996f7d
Matthew Markwell <markwell@google.com>
2024-07-20 11:03:50 -0700

    Don't autofold... Still haven't figured out how I want to use folds.

--- nixpkgs/home-manager/modules/nvim-kickstart/init.lua
+++ nixpkgs/home-manager/modules/nvim-kickstart/init.lua
@@ -178,7 +178,7 @@ vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 --vim.opt.foldcolumn = '0'
 --vim.opt.foldtext = ''
 vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 1
+--vim.opt.foldlevelstart = 1
 vim.opt.foldnestmax = 2
 
 -- Don't load built-in plugins.
