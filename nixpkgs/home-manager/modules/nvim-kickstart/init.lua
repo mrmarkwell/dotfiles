bf766291fab7f531b6e298ed20dc6d7d9a58c359
Matthew Markwell <markwell@google.com>
2024-08-06 13:28:46 -0700

    Added markdown and proto as unsupported for documentHighlight. Added 'path' sources to cmdline cmp

--- nixpkgs/home-manager/modules/nvim-kickstart/init.lua
+++ nixpkgs/home-manager/modules/nvim-kickstart/init.lua
@@ -1024,7 +1024,7 @@ require('lazy').setup({
               buffer = event.buf,
               callback = function()
                 -- Add filetypes here if they don't support documentHighlight
-                local unsupported_filetypes = { 'pbtxt', 'bzl' }
+                local unsupported_filetypes = { 'pbtxt', 'bzl', 'markdown', 'proto' }
                 local current_filetype = vim.bo[event.buf].filetype
                 if not contains(unsupported_filetypes, current_filetype) then
                   vim.lsp.buf.document_highlight()
@@ -1479,13 +1479,22 @@ require('lazy').setup({
         },
       })
 
-      -- Completion suggestions for execution commands.
       cmp.setup.cmdline(':', {
         mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'cmdline', max_item_count = 10 },
-        },
+        sources = cmp.config.sources({
+          { name = 'path' },
+        }, {
+          { name = 'cmdline' },
+        }),
       })
+
+      -- Completion suggestions for execution commands.
+      -- cmp.setup.cmdline(':', {
+      --   mapping = cmp.mapping.preset.cmdline(),
+      --   sources = {
+      --     { name = 'cmdline', max_item_count = 10 },
+      --   },
+      -- })
 
       -- Completion suggestions for searches.
       cmp.setup.cmdline('/', {
@@ -1778,59 +1787,59 @@ require('lazy').setup({
     main = 'ibl',
     opts = {},
   },
-  {
-    -- Linting
-    'mfussenegger/nvim-lint',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      -- local lint = require 'lint'
-      -- lint.linters_by_ft = {
-      --   markdown = { 'markdownlint' },
-      -- }
-      --
-      -- To allow other plugins to add linters to require('lint').linters_by_ft,
-      -- instead set linters_by_ft like this:
-      -- lint.linters_by_ft = lint.linters_by_ft or {}
-      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
-      --
-      -- However, note that this will enable a set of default linters,
-      -- which will cause errors unless these tools are available:
-      -- {
-      --   clojure = { "clj-kondo" },
-      --   dockerfile = { "hadolint" },
-      --   inko = { "inko" },
-      --   janet = { "janet" },
-      --   json = { "jsonlint" },
-      --   markdown = { "vale" },
-      --   rst = { "vale" },
-      --   ruby = { "ruby" },
-      --   terraform = { "tflint" },
-      --   text = { "vale" }
-      -- }
-      --
-      -- You can disable the default linters by setting their filetypes to nil:
-      -- lint.linters_by_ft['clojure'] = nil
-      -- lint.linters_by_ft['dockerfile'] = nil
-      -- lint.linters_by_ft['inko'] = nil
-      -- lint.linters_by_ft['janet'] = nil
-      -- lint.linters_by_ft['json'] = nil
-      -- lint.linters_by_ft['markdown'] = nil
-      -- lint.linters_by_ft['rst'] = nil
-      -- lint.linters_by_ft['ruby'] = nil
-      -- lint.linters_by_ft['terraform'] = nil
-      -- lint.linters_by_ft['text'] = nil
-
-      -- Create autocommand which carries out the actual linting
-      -- on the specified events.
-      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-        group = lint_augroup,
-        callback = function()
-          require('lint').try_lint()
-        end,
-      })
-    end,
-  },
+  --  {
+  --    -- Linting
+  --    'mfussenegger/nvim-lint',
+  --    event = { 'BufReadPre', 'BufNewFile' },
+  --    config = function()
+  --      -- local lint = require 'lint'
+  --      -- lint.linters_by_ft = {
+  --      --   markdown = { 'markdownlint' },
+  --      -- }
+  --      --
+  --      -- To allow other plugins to add linters to require('lint').linters_by_ft,
+  --      -- instead set linters_by_ft like this:
+  --      -- lint.linters_by_ft = lint.linters_by_ft or {}
+  --      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
+  --      --
+  --      -- However, note that this will enable a set of default linters,
+  --      -- which will cause errors unless these tools are available:
+  --      -- {
+  --      --   clojure = { "clj-kondo" },
+  --      --   dockerfile = { "hadolint" },
+  --      --   inko = { "inko" },
+  --      --   janet = { "janet" },
+  --      --   json = { "jsonlint" },
+  --      --   markdown = { "vale" },
+  --      --   rst = { "vale" },
+  --      --   ruby = { "ruby" },
+  --      --   terraform = { "tflint" },
+  --      --   text = { "vale" }
+  --      -- }
+  --      --
+  --      -- You can disable the default linters by setting their filetypes to nil:
+  --      -- lint.linters_by_ft['clojure'] = nil
+  --      -- lint.linters_by_ft['dockerfile'] = nil
+  --      -- lint.linters_by_ft['inko'] = nil
+  --      -- lint.linters_by_ft['janet'] = nil
+  --      -- lint.linters_by_ft['json'] = nil
+  --      -- lint.linters_by_ft['markdown'] = nil
+  --      -- lint.linters_by_ft['rst'] = nil
+  --      -- lint.linters_by_ft['ruby'] = nil
+  --      -- lint.linters_by_ft['terraform'] = nil
+  --      -- lint.linters_by_ft['text'] = nil
+  --
+  --      -- Create autocommand which carries out the actual linting
+  --      -- on the specified events.
+  --      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+  --      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+  --        group = lint_augroup,
+  --        callback = function()
+  --          require('lint').try_lint()
+  --        end,
+  --      })
+  --    end,
+  --  },
   -- NOTE: markwell's plugins start here (stuff not included in kickstart)
   {
     -- Toggle formatted markdown view with :RenderMarkdownToggle
@@ -1839,7 +1848,8 @@ require('lazy').setup({
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     config = function()
       require('render-markdown').setup({})
-      vim.keymap.set('n', '<leader>md', '<Cmd>RenderMarkdownToggle<CR>', { desc = 'Toggle [M]ark[d]own' })
+      -- This option no longer exists.
+      --vim.keymap.set('n', '<leader>md', '<Cmd>RenderMarkdownToggle<CR>', { desc = 'Toggle [M]ark[d]own' })
     end,
   },
   {
