4e28844ab389595184e1d31daf7f5cea178df884
Matthew Markwell <matthewmarkwell@gmail.com>
2025-07-23 10:31:18 -0700

    format with lsp

--- config_rewrite_2025/.config/nvim/init.lua
+++ config_rewrite_2025/.config/nvim/init.lua
@@ -57,7 +57,7 @@ end
 
 -- Shorter version for visual keymaps.
 local vmap = function(lhs, rhs, desc)
-	map('v', lhs, rhs, desc)
+  map('v', lhs, rhs, desc)
 end
 
 -- Function to open the most recent N buffers from vim.v.oldfiles
@@ -152,14 +152,14 @@ vim.g.loaded_zipPlugin = true
 ------------------------ Options ---------------------------
 
 vim.opt.clipboard = "unnamedplus" -- Yank to system clipboard.
-vim.opt.showmode = false -- Don't display mode (its in status line).
-vim.opt.mouse = 'a' -- Enable mouse mode.
-vim.opt.nu = true -- Line numbers.
-vim.opt.relativenumber = true -- Relative line numbers.
-vim.opt.backup = false -- ciderlsp doesn't work well with backups.
+vim.opt.showmode = false          -- Don't display mode (its in status line).
+vim.opt.mouse = 'a'               -- Enable mouse mode.
+vim.opt.nu = true                 -- Line numbers.
+vim.opt.relativenumber = true     -- Relative line numbers.
+vim.opt.backup = false            -- ciderlsp doesn't work well with backups.
 vim.opt.writebackup = false
-vim.opt.updatetime = 250 -- Don't wait 4s to trigger CursorHold.
-vim.opt.timeoutlen = 250 -- Display which-key faster.
+vim.opt.updatetime = 250          -- Don't wait 4s to trigger CursorHold.
+vim.opt.timeoutlen = 250          -- Display which-key faster.
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
@@ -182,18 +182,18 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = 'yes'
 vim.opt.isfname:append('@-@')
 vim.opt.colorcolumn = '81' -- Putting this at 81 since the 80 is the limit.
-vim.opt.autochdir = true -- Change cwd every time moving to buffer.
-vim.opt.splitright = true -- Horizontal splits to the right.
-vim.opt.splitbelow = true -- Vertical splits below.
+vim.opt.autochdir = true   -- Change cwd every time moving to buffer.
+vim.opt.splitright = true  -- Horizontal splits to the right.
+vim.opt.splitbelow = true  -- Vertical splits below.
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.inccommand = 'split' -- Preview substitutions in real time.
-vim.opt.cursorline = true -- Show line with cursor.
+vim.opt.cursorline = true    -- Show line with cursor.
 
 ------------------------ Keymaps ---------------------------
 
 nmap("<leader><leader>x", "<cmd>source %<CR>", "Source this file")
-nmap("<leader>x", ":.lua<CR>", "Source this line") 
+nmap("<leader>x", ":.lua<CR>", "Source this line")
 vmap("<leader>x", ":lua<CR>", "Source visual selection")
 nmap('<leader>y', ':%y<CR>', '[Y]ank entire file')
 nmap('<leader>q', '<cmd>bp|bd #<CR>', 'Close current buffer')
@@ -266,7 +266,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -275,7 +275,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.lsp.enable({"clangd", "lua_ls"})
+vim.lsp.enable({ "clangd", "lua_ls" })
 
 -- Setup lazy.nvim
 require("lazy").setup({
@@ -284,74 +284,74 @@ require("lazy").setup({
     --       `require('ThePlugin').setup({...})`
 
     -- "gc" to comment highlighted selection.
-    { 'numToStr/Comment.nvim', opts = {} },
+    { 'numToStr/Comment.nvim',           opts = {} },
     -- TODO: Setup blink.cmp. Treesitter.
     -- LSP basic setup.
     {
-    "folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
       },
     },
-  },
-  {
-  'saghen/blink.cmp',
-  -- optional: provides snippets for the snippet source
-  dependencies = { 'rafamadriz/friendly-snippets' },
+    {
+      'saghen/blink.cmp',
+      -- optional: provides snippets for the snippet source
+      dependencies = { 'rafamadriz/friendly-snippets' },
 
-  -- use a release tag to download pre-built binaries
-  version = '1.*',
-  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  -- build = 'cargo build --release',
-  -- If you use nix, you can build from source using latest nightly rust with:
-  -- build = 'nix run .#build-plugin',
+      -- use a release tag to download pre-built binaries
+      version = '1.*',
+      -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+      -- build = 'cargo build --release',
+      -- If you use nix, you can build from source using latest nightly rust with:
+      -- build = 'nix run .#build-plugin',
 
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
-    --
-    -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = 'default' },
+      ---@module 'blink.cmp'
+      ---@type blink.cmp.Config
+      opts = {
+        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+        -- 'super-tab' for mappings similar to vscode (tab to accept)
+        -- 'enter' for enter to accept
+        -- 'none' for no mappings
+        --
+        -- All presets have the following mappings:
+        -- C-space: Open menu or open docs if already open
+        -- C-n/C-p or Up/Down: Select next/previous item
+        -- C-e: Hide menu
+        -- C-k: Toggle signature help (if signature.enabled = true)
+        --
+        -- See :h blink-cmp-config-keymap for defining your own keymap
+        keymap = { preset = 'default' },
 
-    appearance = {
-      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'mono'
+        appearance = {
+          -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+          -- Adjusts spacing to ensure icons are aligned
+          nerd_font_variant = 'mono'
+        },
+
+        -- (Default) Only show the documentation popup when manually triggered
+        completion = { documentation = { auto_show = false } },
+
+        -- Default list of enabled providers defined so that you can extend it
+        -- elsewhere in your config, without redefining it, due to `opts_extend`
+        sources = {
+          default = { 'lsp', 'path', 'snippets', 'buffer' },
+        },
+
+        -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+        -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+        -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+        --
+        -- See the fuzzy documentation for more information
+        fuzzy = { implementation = "prefer_rust_with_warning" }
+      },
+      opts_extend = { "sources.default" }
     },
-
-    -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
-
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-
-    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-    --
-    -- See the fuzzy documentation for more information
-    fuzzy = { implementation = "prefer_rust_with_warning" }
-  },
-  opts_extend = { "sources.default" }
-},
-  {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
+    { "nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
