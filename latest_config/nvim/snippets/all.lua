9d45040a5103ea4089ac620450f2b8e1559c1f43
Matthew Markwell <markwell@google.com>
2025-09-05 17:35:20 +0000

    neovim update

--- latest_config/nvim/snippets/all.lua
+++ latest_config/nvim/snippets/all.lua
@@ -2,6 +2,51 @@ local fmt = require('luasnip.extras.fmt').fmt
 return {
   -- important! fmt does not return a snippet, it returns a table of nodes.
   s(
+    'BEGIN_PUBLIC',
+    fmt(
+      [[
+      BEGIN_PUBLIC
+
+      {}
+
+      END_PUBLIC
+
+  ]],
+      {
+        -- i(1) is at nodes[1]
+        i(1, 'Internal change'),
+      }
+    )
+  ),
+  s('BUG', t('BUG=427983349')),
+  s('NO_BUG', t('NO_BUG=no bug')),
+  s(
+    'TODO',
+    fmt(
+      [[
+      // TODO: b/{} - 
+      ]],
+      {
+        -- i(1) is at nodes[1]
+        i(1, '427983349'),
+      }
+    )
+  ),
+  s('BYPASS', t('BYPASS_AICORE_GUITAR=no affected tests.')),
+  s(
+    'MARKWELL',
+    fmt(
+      [[
+      int dbg_counter = 0;
+      VLOG(1) << "MARKWELLDEBUG: {}" << dbg_counter++;
+      ]],
+      {
+        -- i(1) is at nodes[1]
+        i(1, 'HERE: '),
+      }
+    )
+  ),
+  s(
     'example1',
     fmt('just an {iNode1}', {
       iNode1 = i(1, 'example'),
@@ -82,50 +127,6 @@ return {
       {},
       {
         indent_string = [[\t]],
-      }
-    )
-  ),
-  s(
-    'BEGIN_PUBLIC',
-    fmt(
-      [[
-      BEGIN_PUBLIC
-
-      {}
-
-      END_PUBLIC
-
-  ]],
-      {
-        -- i(1) is at nodes[1]
-        i(1, 'Internal change'),
-      }
-    )
-  ),
-  s('BUG', t('BUG=411727687')),
-  s(
-    'TODO',
-    fmt(
-      [[
-      // TODO: b/{} - 
-      ]],
-      {
-        -- i(1) is at nodes[1]
-        i(1, '411727687'),
-      }
-    )
-  ),
-  s('BYPASS', t('BYPASS_AICORE_GUITAR=')),
-  s(
-    'MARKWELL',
-    fmt(
-      [[
-      int dbg_counter = 0;
-      VLOG(1) << "MARKWELLDEBUG: {}" << dbg_counter++;
-      ]],
-      {
-        -- i(1) is at nodes[1]
-        i(1, 'HERE: '),
       }
     )
   ),
